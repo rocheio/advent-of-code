99a3643b8e20529f576ff0c3758cd669c3a84503
Andy Roche <andy@roche.io>
2019-12-01 12:04:32 -0500

    recursive solution day 1 part 2

--- day1.py
+++ day1.py
@@ -5,12 +5,10 @@ https://adventofcode.com/2019/day/1
 
 def fuel_needed(mass):
     """Return total mass of fuel required to launch a mass including added fuel."""
-    total_fuel = 0
-    new_fuel = fuel_calc(mass)
-    while new_fuel > 0:
-        total_fuel += new_fuel
-        new_fuel = fuel_calc(new_fuel)
-    return total_fuel
+    fuel_for_mass = fuel_calc(mass)
+    if fuel_for_mass < 0:
+        return 0
+    return fuel_for_mass + fuel_needed(fuel_for_mass)
 
 
 def fuel_calc(mass):
