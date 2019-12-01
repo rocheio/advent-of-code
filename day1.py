c489e9fbd55604ed3c19704da4adef522d7e6c15
Andy Roche <andy@roche.io>
2019-12-01 11:43:24 -0500

    complete day 1 part 2

--- day1.py
+++ day1.py
@@ -1,37 +1,28 @@
 """
-Fuel required to launch a given module is based on its mass.
-
-Specifically, to find the fuel required for a module:
-
-- take its mass
-- divide by three
-- round down
-- and subtract 2.
-
-For example:
-
-- For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
-- For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
-- For a mass of 1969, the fuel required is 654.
-- For a mass of 100756, the fuel required is 33583.
-
-The Fuel Counter-Upper needs to know the total fuel requirement.
-To find it, individually calculate the fuel needed for the mass of each
-module (your puzzle input), then add together all the fuel values.
-
-What is the sum of the fuel requirements for all of the modules on your spacecraft?
+https://adventofcode.com/2019/day/1
 """
 
 
 def fuel_needed(mass):
+    """Return total mass of fuel required to launch a mass including added fuel."""
+    total_fuel = 0
+    new_fuel = fuel_calc(mass)
+    while new_fuel > 0:
+        total_fuel += new_fuel
+        new_fuel = fuel_calc(new_fuel)
+    return total_fuel
+
+
+def fuel_calc(mass):
+    """Return mass of fuel directly needed to launch a mass."""
     return int(mass / 3) - 2
 
 
 def test():
     assert fuel_needed(12) == 2
     assert fuel_needed(14) == 2
-    assert fuel_needed(1969) == 654
-    assert fuel_needed(100756) == 33583
+    assert fuel_needed(1969) == 966
+    assert fuel_needed(100756) == 50346
 
 
 def main():
