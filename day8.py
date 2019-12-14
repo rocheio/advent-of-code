144b424827a0d1453cf8943689c455002edda169
Andy Roche <andy@roche.io>
2019-12-14 14:37:42 -0500

    complete day 8 part 2

--- day8.py
+++ day8.py
@@ -2,10 +2,50 @@
 https://adventofcode.com/2019/day/8
 """
 
+# Color constants
+BLACK = "0"
+WHITE = "1"
+TRANSPARENT = "2"
+
 
 def chunks(iterable, n):
     """Return list of iterable split into list of evenly n-sized iterables."""
     return [iterable[i:i+n] for i in range(0, len(iterable), n)]
+
+
+def decode_image(data, width, height):
+    """Return a list of strings representing the fully decoded image."""
+    layers = [chunks(layer, width) for layer in chunks(data, width*height)]
+    output = []
+    # Process all layers for each cell in the image
+    for row in zip(*layers):
+        output_row = ""
+        for cell in zip(*row):
+            output_row += decode_cell_layers(cell)
+        output += [output_row]
+    return output
+
+
+def decode_cell_layers(cell):
+    """Return the processed image color for a cell.
+    Cell data is a tuple of string integers for each layer from top-to-bottom.
+    """
+    for pixel in cell:
+        if pixel == TRANSPARENT:
+            continue
+        return pixel
+    return TRANSPARENT
+
+
+def test():
+    data = "0222112222120000"
+    width = 2
+    height = 2
+    want = [
+        "01",
+        "10"
+    ]
+    assert decode_image(data, width, height) == want
 
 
 def main():
@@ -14,15 +54,11 @@ def main():
 
     width = 25
     height = 6
-
-    layers = chunks(data, width*height)
-    count_zeroes = {
-        image: image.count("0") for image in layers
-    }
-    fewest = min(count_zeroes, key=count_zeroes.get)
-    answer = fewest.count("1") * fewest.count("2")
-    print(f"fewest zero layer has {answer} ones and twos")
+    image = decode_image(data, width, height)
+    for row in image:
+        print(row.replace(BLACK, " "))
 
 
 if __name__ == "__main__":
+    test()
     main()
